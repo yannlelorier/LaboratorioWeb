efb72aee38f83e588e457eabb9190ca8a0ae292a
yannlelorier <yannllb97@gmail.com>
2021-08-31 21:33:01 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -8,17 +8,45 @@ function App() {
   
   const [date, setDate] = useState(new Date());
   const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
 
   
   useEffect(() => {
     let interval = null;
     
       interval = setInterval(() => {
-        setSeconds(seconds => seconds + 1);
-      }, 1000);
+        setSeconds(setSeconds(
+          if(seconds < 59) {
+            seconds => seconds + 1;
+          }else if () {
+            seconds => 0;
+            setMinutes(minutes => minutes + 1);
+          }
+        ), 1000);
     
     return () => clearInterval(interval);
-  }, [isActive, seconds]);
+  });
+
+  useEffect(() => {
+    let interval = null;
+    
+      interval = setInterval(() => {
+        setMinutes(minutes => minutes<59? minutes + 1 : 0);
+      }, 6000);
+    
+    return () => clearInterval(interval);
+  });
+
+  useEffect(() => {
+    let interval = null;
+    
+      interval = setInterval(() => {
+        setHours(hours => hours + 1);
+      }, 360000);
+    
+    return () => clearInterval(interval);
+  });
 
 
 
@@ -38,7 +66,7 @@ function App() {
 
   return (
     <div>
-      {seconds}
+      {hours}h-{minutes}m-{seconds}s
       <h2>It is {date.toLocaleTimeString()}.</h2>
       
     </div>
