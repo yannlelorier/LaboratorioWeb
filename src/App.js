b4bf8100ea7061e59816f4eb325d729f486aa84a
yannlelorier <yannllb97@gmail.com>
2021-08-31 21:04:56 -0500

    changin

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
 //librerias ó components
 import "./App.css";
 import "./index.css";
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect } from "react";
+import NewInput from "./components/NewInput";
 
 function App() {
   
   const [users, setUsers] = useState([{ id: 0, name: "Ruben" }]);
 
-  const onClick = () => {};  
+  const onClick = () => {
+    //... := spread notation
+    setUsers([...users, {id:users.length, name:"Yann"}])
+  };
+
+  const [date, setDate] = useState(new Date());
+  const [seconds, setSeconds] = useState(0)
+
+  useEffect (() => {
+    var timerID = setInterval(() => tick(), 1000);
+
+    //limpiando los threads
+    return function cleanup(params) {
+      clearInterval(timerID)
+    }
+  });
+
+  useEffect (() => {
+    let interval = null;
+
+    interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);}, 1000);
+
+      return function cleanup(params) {
+        clearInterval(seconds)
+      }
+  })
+
+  function tick() {
+    setDate(new Date())
+  }
+
+  // de aqui heredan todos los componentes que los usen
+  var dataComponent = {
+    type:"button",
+    value:"Update"
+  };
 
   return (
     <div>
-      <input type="button" onClick={onClick} value="Update" />
+      {/* primero trata de renderear date */}
+      <h1>{seconds}</h1>
+      <h1>{date.toLocaleTimeString()}</h1>
+      {/* otro spread notation */}
+      <NewInput onClick={onClick} {...dataComponent} />
       <div>
         {users.map((e) => (
           <div>
