e6d2bb925b839ed972d8e0d3d58ea95971e69959
yannlelorier <yannllb97@gmail.com>
2021-09-03 19:31:40 -0500

    adding the things todo

--- src/components/NewInput.jsx
+++ src/components/NewInput.jsx
@@ -4,14 +4,22 @@ function Home() {
     return (
       <div>
         <h2>Home</h2>
+        <h3>A clock for your convenience</h3>
       </div>
     );
   }
   
   function About() {
+    var dataComponent = {
+      type:"button",
+      value:"Update"
+    }
+
     return (
       <div>
         <h2>About</h2>
+        <h3>Our workers</h3>
+        <input {...dataComponent}/>
       </div>
     );
   }
@@ -20,6 +28,7 @@ function Home() {
     return (
       <div>
         <h2>Dashboard</h2>
+        <h3>a Timer! For your convenience</h3>
       </div>
     );
   }
